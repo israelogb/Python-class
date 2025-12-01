30d387381609d16ccc262043a93f016de9ec456e
israelogb <isrealogbedobor@gmail.com>
2025-12-01 16:27:06 +0100

    Add files via upload

---
+++ caculating vowels on a sentence.py
@@ -0,0 +1,7 @@
+#vowels total in a sentence
+st=input("enter a string:")
+vow=0
+for i in st:
+    if (i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U' ):
+        vow=vow
+        print('number of vowel in string:',vow)
